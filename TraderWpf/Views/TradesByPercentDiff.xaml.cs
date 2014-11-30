30100f0497e3128bb39c84eef99f4753a9dfbab4
RolandPheasant <roland_pheasant@hotmail.com>
2014-11-30 19:17:54 +0000

    Group by example

---
+++ TraderWpf/Views/TradesByPercentDiff.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TraderWpf.Views
+{
+    /// <summary>
+    /// Interaction logic for TradesByPercentDiff.xaml
+    /// </summary>
+    public partial class TradesByPercentDiff : UserControl
+    {
+        public TradesByPercentDiff()
+        {
+            InitializeComponent();
+        }
+    }
+}
