cee4b4441efd37673af1dd4533246ed4cc1f8b91
Roland Pheasant <roland_pheasant@hotmail.com>
2015-02-16 18:25:26 +0000

    Live log tile

--- Trader.App/Infrastucture/MenuItem.cs
+++ Trader.App/Infrastucture/MenuItem.cs
@@ -16,20 +16,26 @@ namespace Trader.Client.Infrastucture
     {
         private readonly string _title;
         private readonly string _description;
+        private readonly object _content;
         private readonly IEnumerable<Link> _link;
         private readonly ICommand _command;
         private readonly MenuCategory _category;
 
-        public MenuItem(string title, string description, Action action,  IEnumerable<Link> link = null)
+        public MenuItem(string title, string description, Action action,  
+            IEnumerable<Link> link = null,
+            object content = null)
         {
             _title = title;
             _description = description;
+            _content = content;
             _category = MenuCategory.DynamicData;
             _link = link ?? Enumerable.Empty<Link>();
             _command = new Command(action); ;
         }
 
-        public MenuItem(string title,string description ,Action action, MenuCategory category,IEnumerable<Link> link = null)
+        public MenuItem(string title,string description ,Action action, MenuCategory category,
+            IEnumerable<Link> link = null,
+            object content=null)
         {
             _title = title;
             _description = description;
@@ -57,6 +63,11 @@ namespace Trader.Client.Infrastucture
         public string Description
         {
             get { return _description; }
+        }
+
+        public object Content
+        {
+            get { return _content; }
         }
     }
 }
