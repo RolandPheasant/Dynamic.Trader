41c420221d0f394a6133892dbd0a2b02b7157201
Roland Pheasant <roland_pheasant@hotmail.com>
2015-03-01 19:32:58 +0000

    Missed file

---
+++ Trader.Client/Views/RxUiHostView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Trader.Client.Views
+{
+    /// <summary>
+    /// Interaction logic for RxUiHostView.xaml
+    /// </summary>
+    public partial class RxUiHostView : UserControl
+    {
+        public RxUiHostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
