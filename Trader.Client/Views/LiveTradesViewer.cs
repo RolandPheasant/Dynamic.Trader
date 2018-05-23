b4952bcbf0e32fedeb07d26077c8267bfce4588a
Roland Pheasant <roland@dynamic-data.org>
2018-05-23 20:24:45 +0100

    Added pause updates option to live trades viewer

--- Trader.Client/Views/LiveTradesViewer.cs
+++ Trader.Client/Views/LiveTradesViewer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using DynamicData;
 using DynamicData.Binding;
 using DynamicData.PLinq;
+using ReactiveUI;
 using Trader.Domain.Model;
 using Trader.Domain.Services;
 
@@ -14,6 +16,7 @@ namespace Trader.Client.Views
     {
         private readonly IDisposable _cleanUp;
         private readonly ReadOnlyObservableCollection<TradeProxy> _data;
+        private bool _paused;
 
         public LiveTradesViewer(ITradeService tradeService,SearchHints searchHints)
         {
@@ -23,10 +26,10 @@ namespace Trader.Client.Views
                         .Select(BuildFilter);
 
             var loader = tradeService.Live.Connect()
+                .BatchIf(this.WhenValueChanged(x=>x.Paused),null,null) //I need to fix the API, so nulls do noy have to be passed in
                 .Filter(filter) // apply user filter
-                //if targeting dotnet 4.5 can parallelise 'cause it's quicker
                 .Transform(trade => new TradeProxy(trade))
-                .Sort(SortExpressionComparer<TradeProxy>.Descending(t => t.Timestamp),SortOptimisations.ComparesImmutableValuesOnly, 25)
+                .Sort(SortExpressionComparer<TradeProxy>.Descending(t => t.Timestamp).ThenByDescending(t=>t.Timestamp),SortOptimisations.ComparesImmutableValuesOnly, 25)
                 .ObserveOnDispatcher()
                 .Bind(out _data)   // update observable collection bindings
                 .DisposeMany() //since TradeProxy is disposable dispose when no longer required
@@ -46,6 +49,13 @@ namespace Trader.Client.Views
         public ReadOnlyObservableCollection<TradeProxy> Data => _data;
 
         public SearchHints SearchHints { get; }
+
+
+        public bool Paused
+        {
+            get => _paused;
+            set => SetAndRaise(ref _paused, value);
+        }
 
         public void Dispose()
         {
